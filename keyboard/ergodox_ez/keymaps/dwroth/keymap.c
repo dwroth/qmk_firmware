4defa7622fb53e2fbb840a6649c5ef8a8a45286c
Daniel Roth <droth@yelp.com>
2016-06-28 15:46:07 -0700

    Fixes word skipping and lock key

--- keyboard/ergodox_ez/keymaps/dwroth/keymap.c
+++ keyboard/ergodox_ez/keymaps/dwroth/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                               KC_BSPC,            KC_DELETE,          KC_END,      
         
         // right hand
-        KC_F16,               KC_6,            KC_7,         KC_8,            KC_9,               KC_0,               KC_MINS,
+        HYPR(KC_F16),               KC_6,            KC_7,         KC_8,            KC_9,               KC_0,               KC_MINS,
         MEH_T(KC_NO),         KC_Y,            KC_U,         KC_I,            KC_O,               KC_P,               KC_BSLS,
                               KC_H,            KC_J,         KC_K,            KC_L,               LT(MDIA, KC_SCLN),  KC_QUOT,
         OSL(SYMB),            KC_N,            KC_M,         KC_COMM,         KC_DOT,             CTL_T(KC_SLSH),     KC_RSFT,
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |PrevWd|NextWd|      |      |        |
+ * |        |      |      |      |      |      |      |           |      |      |      |PrevWd|NextWd|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |------|           |------| Left | Down |  Up  | Right|      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -153,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 |Space |      |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -166,10 +166,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
+                                  KC_SPC,  KC_TRNS, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, LALT(KC_LEFT), RALT(KC_RIGHT), KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, LALT(KC_LEFT), RALT(KC_RIGHT), KC_TRNS, KC_TRNS,
                  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS, 
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                           HYPR(KC_LEFT), HYPR(KC_RIGHT), KC_TRNS, KC_TRNS, KC_TRNS,
